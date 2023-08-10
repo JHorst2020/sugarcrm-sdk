9a6dc3b56aa25f7ce2a30092e3c5ffe62bee5910
JHorst2020 <jerzyhorst@gmail.com>
2023-08-10 16:37:54 -0700

    Modified export syntax

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
-"use strict";
-require('dotenv').config();
-exports = module.exports = require('./src/app');
+import dotenv from 'dotenv';
+import app from './src/app';
+
+dotenv.config();
+
+// Deconstructing the app and exporting it as both default and named
+const { ...myApp } = app;
+
+export {
+  myApp as default,
+  myApp as app
+};
